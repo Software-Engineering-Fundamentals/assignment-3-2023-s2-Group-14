6e8515779097c49064a067080a66f5ac9ef93528
Esmat5 <s3941885@student.rmit.edu.au>
2023-09-25 09:31:11 +1000

    created user class parameters

--- src/com/esmat/User.java
+++ src/com/esmat/User.java
@@ -1,5 +1,55 @@
 package com.esmat;
 
-public class User {
+import java.util.Date;
 
+public class User {
+    private String userID;
+    private String name;
+    private String email;
+    private String address;
+    private Date dateOfBirth;
+    private String phoneNumber;
+    private String emergencyContact;
+    private String role;
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmergencyContact() {
+        return emergencyContact;
+    }
+
+    public void setEmergencyContact(String emergencyContact) {
+        this.emergencyContact = emergencyContact;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
