c6ce0ff3d18999485dc8dbbf16ceef9578fafd2c
Esmat5 <s3941885@student.rmit.edu.au>
2023-09-25 09:50:54 +1000

    created userRegistration function

--- src/com/esmat/User.java
+++ src/com/esmat/User.java
@@ -1,17 +1,38 @@
 package com.esmat;
 
+import java.security.cert.TrustAnchor;
 import java.util.Date;
 
 public class User {
     private String userID;
-    private String name;
+    private String firstName;
+    private String lastName;
     private String email;
     private String address;
     private Date dateOfBirth;
     private String phoneNumber;
     private String emergencyContact;
+    private String password;
     private String role;
 
+
+
+
+    //This is the user registration function
+    public Boolean registerAccount(String name, String email, String address, Date doB, String phoneNumber,
+                             String emergencyContact, String password){
+        return true;
+    }
+
+    //This is the login function
+    public Boolean login(String email, String password){
+        return true;
+    }
+
+    //This is the reset user's password function
+    public boolean resetPassword(String oldPassword, String newPassword){
+        return true;
+    }
 
     public String getEmail() {
         return email;
@@ -51,5 +72,13 @@ public class User {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
