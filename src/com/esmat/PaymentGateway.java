e152bec965e81ecb1abe4bfbc6ca36c8780c3927
Esmat5 <s3941885@student.rmit.edu.au>
2023-09-25 11:13:43 +1000

    created paymentGateway class

--- src/com/esmat/PaymentGateway.java
+++ src/com/esmat/PaymentGateway.java
@@ -1,2 +1,11 @@
-package com.esmat;public class PaymentGateway {
+package com.esmat;
+
+public class PaymentGateway {
+    private String paymentID;
+    private String transactionDetails;
+    private String userID;
+    private double paymentCost;
+
+
+
 }
