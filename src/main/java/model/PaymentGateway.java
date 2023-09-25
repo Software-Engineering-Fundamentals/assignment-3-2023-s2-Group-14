78227981c239aa99483cb1c1247f56f46e0b75c9
s3791873 <s3791873@student.rmit.edu.au>
2023-09-25 20:16:27 +1000

    package and import updates

---
+++ src/main/java/model/PaymentGateway.java
@@ -0,0 +1,9 @@
+package main.java.model;
+
+public class PaymentGateway {
+    private String paymentID;
+    private String transactionDetails;
+    private String userID;
+    private double totalCost;
+
+}
