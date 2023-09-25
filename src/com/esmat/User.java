f07f52b0d4ddb18a4a4dee2e934a8697b3016c4d
Esmat5 <s3941885@student.rmit.edu.au>
2023-09-25 10:12:18 +1000

    implemented user registation function

--- src/com/esmat/User.java
+++ src/com/esmat/User.java
@@ -19,9 +19,23 @@ public class User {
 
 
     //This is the user registration function
-    public Boolean registerAccount(String name, String email, String address, Date doB, String phoneNumber,
+    public Boolean registerAccount(String firstName, String lastName, String email, String address, Date doB, String phoneNumber,
                              String emergencyContact, String password){
-        return true;
+
+        /*
+        If firstName, lastName, email, and password fields are not empty then we can create an account for the user
+         */
+        if (!firstName.isEmpty() && !lastName.isEmpty() && !email.isEmpty() && !password.isEmpty()){
+            System.out.println("Account was successfully created");
+            return true;
+        }
+
+        /*
+        If firstName, lastName, email, and password fields are empty, cannot create an account for the user and
+        returns false
+         */
+        System.out.println("Account was not successfully created\nPleas fill all the fields");
+        return false;
     }
 
     //This is the login function
