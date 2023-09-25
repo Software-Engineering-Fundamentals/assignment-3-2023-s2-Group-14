5da8e6a1808090c37d9411a7c21347af40bcf55d
s3791873 <s3791873@student.rmit.edu.au>
2023-09-25 20:16:04 +1000

    Enquiry class

---
+++ src/main/java/model/Enquiry.java
@@ -0,0 +1,63 @@
+package main.java.model;
+
+public class Enquiry {
+    private String enquiryID;
+    private String userID;
+    private String content;
+    private boolean resolved;
+
+    // Constructor
+    public Enquiry(String enquiryID, String userID, String content, boolean resolved) {
+        this.enquiryID = enquiryID; // Will use the same enquiryID for simplicity.
+        this.userID = userID;
+        this.content = content;
+        this.resolved = resolved;
+        System.out.println("\nYou entered: " + content
+                + "\nSystem is lodging.....\nSuccessfully lodged..!. Please be patient while we handle your enquiry\n");
+    }
+
+    // Getter method for userID
+    public String enquiryID() {
+        return enquiryID;
+    }
+
+    // Getter method for userID
+    public String getUserID() {
+        return userID;
+    }
+
+    // Getter method for content
+    public String getContent() {
+        return content;
+    }
+
+    // Setter method for content
+    public void setContent(String addContents) {
+        this.content = content + "\n------------------------------ \n" + addContents;
+
+    }
+
+    // Getter method for resolved
+    public boolean isResolved() {
+        return resolved;
+    }
+
+    // Setter method for resolved
+    public void setResolved(boolean resolved) {
+        if (resolved == true) {
+            this.resolved = resolved;
+
+        } else {
+            System.out.print("The enquiry " + enquiryID + " is unresolved and  requires more information");
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Enquiry ID: " + enquiryID +
+                "\nUser ID: " + userID +
+                "\nEnquiry Query:\n" + content +
+                "\n\nResolved: " + (resolved ? "Yes" : "No");
+    }
+}
