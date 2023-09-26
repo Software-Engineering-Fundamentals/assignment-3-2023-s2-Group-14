30147e68b1ff4ff00e5c7bbbb00ab5a6759a9fce
s3791873 <s3791873@student.rmit.edu.au>
2023-09-26 20:47:29 +1000

    Course

--- src/main/java/model/Course.java
+++ src/main/java/model/Course.java
@@ -1,5 +1,21 @@
-package java.model;
+package main.java.model;
 
 public class Course {
+    private String courseId;
+    private String title;
+    private String description;
+    private int duration;
+    private String instructor;
+    private String category;
+    private double cost;
 
 }
+
+   public Course(String courseId, String title, String instructor, String category, double cost) {
+        this.courseId = courseId;
+        this.title = title;
+        this.description = description;
+        this.instructor = instructor;
+        this.category = category;
+        this.cost = cost;
+    }
