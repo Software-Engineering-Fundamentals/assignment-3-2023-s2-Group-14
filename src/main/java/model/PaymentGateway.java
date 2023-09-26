d4afff28526db7ddd3bfb98bcf28ae422cff393d
s3791873 <s3791873@student.rmit.edu.au>
2023-09-26 20:47:06 +1000

    Payment Methods

--- src/main/java/model/PaymentGateway.java
+++ src/main/java/model/PaymentGateway.java
@@ -6,4 +6,41 @@ public class PaymentGateway {
     private String userID;
     private double totalCost;
 
-}
+    public PaymentGateway(String paymentID, String transactionDetails, String userID, double totalCost) {
+        this.paymentID = paymentID;
+        this.transactionDetails = transactionDetails;
+        this.userID = userID;
+        this.totalCost = totalCost;
+    }
+
+    public String getPaymentID() {
+        return paymentID;
+    }
+
+    public void setPaymentID(String paymentID) {
+        this.paymentID = paymentID;
+    }
+
+    public String getTransactionDetails() {
+        return transactionDetails;
+    }
+
+    public void setTransactionDetails(String transactionDetails) {
+        this.transactionDetails = transactionDetails;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void addTotalCost(double courseCost) {
+        this.totalCost += courseCost;
+    }
