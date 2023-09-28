3fbdcb210ed46ce32b774091288a97e7744353ed
Esmat5 <s3941885@student.rmit.edu.au>
2023-09-28 13:50:27 +1000

    added enroll and access content funtions for student

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,6 +1,22 @@
 package main.java.model;
+import main.java.model.Course;
 public class Student extends User {
+    Course course = new Course();
+
     public Student(String userID, String firstName, String lastName, String email, String password) {
         super(userID, firstName, lastName, email, password);
     }
+
+    public void enrollCourse(String courseName) {
+        if (courseName.equals(course.getTitle)){
+            System.out.println(getFirstName() + " Enrolled in " + course.getTitle());
+        }
+    }
+
+    public void accessCourseContent(String courseName){
+        if (courseName.equals(course.getTitle)){
+            System.out.println("Course Content accessed:\nBy "+ this.getFirstName());
+        }
+    }
+
 }
